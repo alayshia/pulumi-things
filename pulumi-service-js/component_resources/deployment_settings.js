d66de9cb25a546f8c335d524518b975fea981150
Alayshia Knighten <alayshia@pulumi.com>
2023-12-22 14:11:50 -0500

    initial
    Signed-off-by: Alayshia Knighten <alayshia@pulumi.com>

--- pulumi-service-js/component_resources/deployment_settings.js
+++ pulumi-service-js/component_resources/deployment_settings.js
@@ -7,7 +7,7 @@ Creates a deployment settings instance, registering the settings passed
 
 class MyDeploymentSettings extends pulumi.ComponentResource {
     constructor(name, args, opts) {
-        super("my:components:MyDeploymentSettings", name, args, opts);
+        super("alayshia:components:MyDeploymentSettings", name, args, opts);
 
         this.settings = new pulumiservice.DeploymentSettings(name, args, { parent: this });
 
